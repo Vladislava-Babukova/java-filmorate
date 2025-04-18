adf6c2ded0cc19e840c00ae8226abce8bfa4e838
Vladislava Babukova <Vlada.409b@gmail.com>
2025-04-18 12:43:45 +0300

    Финальное задание 10 спринта.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,33 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.util.List;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final FilmHandler filmHandler = new FilmHandler();
+
+    @PostMapping
+    public Film create(@Valid @RequestBody Film film){
+        log.info("Начато создание фильма.Получен объект{}", film);
+        return filmHandler.create(film);
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film film){
+        log.info("Начато обновление фильма.Получен объект{}", film);
+        return filmHandler.update(film);
+    }
+
+    @GetMapping
+    public List<Film> getAllFilms(){
+        return filmHandler.getAllFilms();
+    }
 }
---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -0,0 +1,75 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.ResourceUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class FilmControllerTest {
+
+    public static final String PATH = "/films";
+
+    @Autowired
+    private MockMvc mockMvc;
+    FilmController filmController;
+
+    @BeforeEach
+    void setUp() {
+        filmController = new FilmController();
+    }
+
+    @Test
+    void create() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getContentFromFile("films/film.json")))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content()
+                        .json(getContentFromFile("films/film.json")));
+    }
+
+    @Test
+    void update() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(getContentFromFile("films/film.json")));
+
+        mockMvc.perform(MockMvcRequestBuilders.put(PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getContentFromFile("films/updateFilm.json")))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content()
+                        .json(getContentFromFile("films/updateFilm.json")));
+    }
+
+    @Test
+    void getAllFilms() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(getContentFromFile("films/film.json")));
+
+        mockMvc.perform(MockMvcRequestBuilders.get(PATH))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+    }
+
+    private String getContentFromFile(String filename) {
+        try {
+            return Files.readString(ResourceUtils.getFile("classpath:" + filename).toPath(), StandardCharsets.UTF_8);
+        } catch (IOException exception) {
+            throw new RuntimeException("Не открывается файл", exception);
+        }
+    }
+}
